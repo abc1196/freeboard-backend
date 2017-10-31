c6e68fc9c730f5caba3cd527e06df71841b5a203
abc1196 <alejob_11@hotmail.com>
2017-10-30 23:53:48 -0500

    Sixth commit

---
+++ src/main/java/com/project/freeboard/util/JWT.java
@@ -0,0 +1,72 @@
+package com.project.freeboard.util;
+
+import java.security.Key;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import javax.crypto.spec.SecretKeySpec;
+
+import com.google.api.server.spi.response.UnauthorizedException;
+import com.project.freeboard.entity.Companies;
+import com.project.freeboard.entity.Students;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+
+public class JWT {
+
+	public static final SignatureAlgorithm SIGNATURE_ALGORITHM = SignatureAlgorithm.HS256;
+
+	public static final byte[] SECRET = "agskgHh2971pA_SaLSDOAaÑSaS[A*sdañSDs:aSX:as!!!;asMAS?MXA:SD=AÑS)JLQ)LS)AJS)AS=Gq=="
+			.getBytes();
+	public static final Key SIGNING_KEY = new SecretKeySpec(SECRET, SIGNATURE_ALGORITHM.getJcaName());
+
+	private String jwt;
+
+	public JWT(String value) {
+		this.jwt = value;
+	}
+
+	public String getValue() {
+		return jwt;
+	}
+
+	public void setValue(String value) {
+		this.jwt = value;
+	}
+
+	/**
+	 * * Generate the access token and its expiration date in order to be used
+	 * within * 10 minutes based on the system's current date time * * @param
+	 * user * The user who will be configured with his access properties
+	 */
+	public static String generateToken(Companies companies) {
+		JwtBuilder builder = Jwts.builder().setId(companies.getEmail())
+				.setIssuedAt(GregorianCalendar.getInstance().getTime()).setExpiration(generateTokenExpiration())
+				.signWith(SIGNATURE_ALGORITHM, SIGNING_KEY);
+		return builder.compact();
+	}
+
+	public static String generateToken(Students student) {
+		JwtBuilder builder = Jwts.builder().setId(student.getEmail())
+				.setIssuedAt(GregorianCalendar.getInstance().getTime()).setExpiration(generateTokenExpiration())
+				.signWith(SIGNATURE_ALGORITHM, SIGNING_KEY);
+		return builder.compact();
+	}
+
+	/**
+	 * * Generate an expiration date adding 10 minutes to the system's current
+	 * date * time * * @return The expiration date
+	 */
+	private static Date generateTokenExpiration() {
+		GregorianCalendar calendar = (GregorianCalendar) GregorianCalendar.getInstance();
+		calendar.add(Calendar.HOUR, 24);
+		return calendar.getTime();
+	}
+
+}
